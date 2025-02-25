db961f2d4b048f7d7e4d386945141a4bd089bc07
Luca <luca@teapot.ovh>
2025-02-25 13:46:11 +0100

    add tentative debian packaging

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,6 @@
+Bug-Database: https://github.com/lucat1/sacme/issues
+Bug-Submit: https://github.com/lucat1/sacme/issues/new
+Changelog: https://github.com/lucat1/sacme/blob/master/CHANGES
+Documentation: https://github.com/lucat1/sacme/wiki
+Repository-Browse: https://github.com/lucat1/sacme
+Repository: https://github.com/lucat1/sacme.git
