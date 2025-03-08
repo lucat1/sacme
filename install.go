969d82a71927078995c8910974ef082f00decf10
Luca <luca@teapot.ovh>
2025-03-08 11:15:53 +0100

    add missing chown to install step

--- install.go
+++ install.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 
 	fs "github.com/spf13/afero"
 )
@@ -76,6 +77,21 @@ func writeFile(f fs.Fs, pp PathPerm, content []byte, installType string) (err er
 
 	if int(l) != len(content) {
 		err = fmt.Errorf("wrote %d bytes, expected to write %d", l, len(content))
+		return
+	}
+
+	uid, err := strconv.Atoi(pp.Owner.Uid)
+	if err != nil {
+		err = fmt.Errorf("could not parse uid %s (user %s) as int: %w", pp.Owner.Uid, pp.Owner.Username, err)
+		return
+	}
+	gid, err := strconv.Atoi(pp.Group.Gid)
+	if err != nil {
+		err = fmt.Errorf("could not parse gid %s (group %s) as int: %w", pp.Group.Gid, pp.Group.Name, err)
+		return
+	}
+	if err = f.Chown(pp.Path, uid, gid); err != nil {
+		err = fmt.Errorf("could not chown %s to %s(%d):%s(%d): %w", pp.Path, pp.Owner.Username, uid, pp.Group.Name, gid, err)
 		return
 	}
 
